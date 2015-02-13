acc2eb844df90bca7fcf35c5be0c572eac5eff21
anthony-ng <thomsonng123@gmail.com>
2015-02-12 23:39:58 -0800

    updated comments

--- main.ctrl.js
+++ main.ctrl.js
@@ -1,3 +1,5 @@
+// Declared the main controller
+// first retrieve the app module, then use the controller function to instantiate a new controller
 angular.module('app').controller("mainController", function() {
   var vm = this;
 });
