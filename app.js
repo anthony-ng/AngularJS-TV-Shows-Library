60fbeb531a67fd0b611d91caed5e4b26a6555586
anthony-ng <thomsonng123@gmail.com>
2015-02-12 23:47:06 -0800

    capture the controller to a variable, then assign all controller variable to this object

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-// instantiate an AngularJS module
+// 1) instantiate an AngularJS module
 // name: 'app'; type: array = to create the module
 angular.module('app', []);
--- main.ctrl.js
+++ main.ctrl.js
@@ -1,7 +1,7 @@
-// Declared the main controller
-// first retrieve the app module, then use the controller function to instantiate a new controller
+// 3) First retrieve the app module, then use the controller function to instantiate a new controller
 // 1st param: conroller name
 // 2nd param: a function to place controller code
 angular.module('app').controller("mainController", function() {
-  var vm = this;
+  var viewModel = this;
+  viewModel.title = 'AngularJS Example'
 });
