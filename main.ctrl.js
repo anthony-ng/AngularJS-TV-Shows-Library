b51438d3a61d0cdf94c0a20df4cbb5c61c30e47b
anthony-ng <thomsonng123@gmail.com>
2015-02-12 23:52:20 -0800

    updated comment

--- main.ctrl.js
+++ main.ctrl.js
@@ -3,7 +3,7 @@
 // 2nd param: a function to place controller code
 angular.module('app').controller("MainController", function() {
 
-  // capture the controller instance to a variable, then assign all controller variable to this object
+  // 5) capture the controller instance to a variable, then assign all controller variable to this object
   var viewModel = this;
   viewModel.title = 'AngularJS Example';
 });
